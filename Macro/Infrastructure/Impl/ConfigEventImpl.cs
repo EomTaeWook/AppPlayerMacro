33ed8c36e0f4eaee77d23f01f9823332042bd970
엄태욱 <trim0917@gmail.com>
2019-01-15 17:44:31 +0900

    네임 스페이스 정리

--- Macro/Infrastructure/Impl/ConfigEventImpl.cs
+++ Macro/Infrastructure/Impl/ConfigEventImpl.cs
@@ -3,10 +3,7 @@ using Macro.Models;
 using Macro.Models.ViewModel;
 using System.Collections.Generic;
 using System.Linq;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
-using System.Windows.Media;
 using Utils;
 
 namespace Macro.View
--- Macro/View/ConfigEventView.xaml.cs
+++ Macro/View/ConfigEventView.xaml.cs
@@ -6,7 +6,6 @@ using MahApps.Metro.Controls;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media;
 using EventType = Macro.Models.EventType;
 
 namespace Macro.View
