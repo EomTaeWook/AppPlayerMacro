8e42d801196cf8eb172cb1bf9c677a71a29d3eee
EomTaeWook <trim0917@gmail.com>
2023-04-07 00:37:48 +0900

    패치노트 변경

--- Macro/Infrastructure/VersionNote.cs
+++ Macro/Infrastructure/VersionNote.cs
@@ -6,7 +6,7 @@ namespace Macro.Infrastructure
     {
         public static readonly int Major = 2;
         public static readonly int Minor = 6;
-        public static readonly int Build = 7;
+        public static readonly int Build = 8;
     }
 
     public class VersionNote
