c816338aaf4c38985f3a4c9af940013c1ab026c0
EomTaeWook <trim0917@gmail.com>
2023-12-17 11:31:01 +0900

    버저닝

--- Macro/Infrastructure/VersionNote.cs
+++ Macro/Infrastructure/VersionNote.cs
@@ -6,7 +6,7 @@ namespace Macro.Infrastructure
     {
         public static readonly int Major = 2;
         public static readonly int Minor = 6;
-        public static readonly int Build = 9;
+        public static readonly int Build = 10;
     }
 
     public class VersionNote
